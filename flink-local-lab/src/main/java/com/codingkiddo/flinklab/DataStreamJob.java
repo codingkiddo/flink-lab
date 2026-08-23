92bba01f008ae3a392597e32a72f831b92f84001
Vinod Kumar <codingkiddo@gmail.com>
2026-08-23 20:07:45 +0530

    Initial commit

---
+++ flink-local-lab/src/main/java/com/codingkiddo/flinklab/DataStreamJob.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codingkiddo.flinklab;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+
+public class DataStreamJob {
+
+	public static void main(String[] args) throws Exception {
+
+		// 1. Create Flink execution environment
+		StreamExecutionEnvironment env =
+				StreamExecutionEnvironment.getExecutionEnvironment();
+
+		// Start with ONE parallel instance
+		env.setParallelism(4);
+
+		// Keep operators separate so they are easier to inspect in Web UI
+		env.disableOperatorChaining();
+
+		// 2. Source
+		DataStream<Long> numbers =
+				env.fromSequence(1, Long.MAX_VALUE)
+						.name("Number Source");
+
+		// 3. Processing operator
+		DataStream<String> processed =
+				numbers
+						.map(new RichMapFunction<Long, String>() {
+
+							@Override
+							public String map(Long value) throws Exception {
+
+								// Deliberately slow processing
+								Thread.sleep(1000);
+
+								int subtask =
+										getRuntimeContext()
+												.getTaskInfo()
+												.getIndexOfThisSubtask();
+
+								return "subtask="
+										+ subtask
+										+ ", value="
+										+ value;
+							}
+						})
+						.name("Slow Processor");
+
+		// 4. Sink
+		processed
+				.print()
+				.name("Console Sink");
+
+		// 5. Submit the job
+		env.execute("Flink Parallelism Lab");
+	}
+}
